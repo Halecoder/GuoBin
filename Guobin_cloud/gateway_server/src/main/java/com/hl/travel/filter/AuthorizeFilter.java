495c57c82f4f7383ca1e1a6d498030e3cbb5aab2
HL <3283152875@qq.com>
2023-06-27 22:26:27 +0800

    完成配置GateWay网关

---
+++ Guobin_cloud/gateway_server/src/main/java/com/hl/travel/filter/AuthorizeFilter.java
@@ -0,0 +1,36 @@
+package com.hl.travel.filter;
+
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Component
+public class AuthorizeFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 1.获取请求参数
+        MultiValueMap<String, String> params = exchange.getRequest().getQueryParams();
+        // 2.校验请求的URL的后缀是否为.do
+        String path = exchange.getRequest().getPath().toString();
+        // 3.校验
+        if (!path.endsWith(".do")) {  // 配置文件重写成功 先执行配置文件的断言，才执行这里的代码 所以这里占个位，没用
+
+            // 放行
+            return chain.filter(exchange);
+        }
+
+        // 4.拦截
+        // 4.1.禁止访问，设置状态码
+        exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+        // 4.2.结束处理
+        return exchange.getResponse().setComplete();
+    }
+}
